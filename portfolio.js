d80d526521623f43a05f21239200b3d698a8da02
Harriet Oteng <harriet.oteng@gmail.com>
2023-03-07 08:48:54 +0000

    Fix linter issues

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,9 @@
+let mobileMenu = document.getElementById('hamburger-icon');
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+mobileMenu.addEventListener('click', () => {
+  toggleMobileMenu(mobileMenu);
+})
