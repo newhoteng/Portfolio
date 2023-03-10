1e94ecb2e848d9db3e90b4a20591a6b9d99c03ed
osugo <josugo26@gmail.com>
2023-03-10 10:39:03 +0300

    Add form validation

--- portfolio.js
+++ portfolio.js
@@ -231,3 +231,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
   inputs.forEach(i => i.addEventListener('input', inputListener));
 });
+
+// validate email
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const error = document.querySelector('.message');
+
+// As per the HTML Specification
+const emailRegExp =
+  /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+// Now we can rebuild our validation constraint
+// Because we do not rely on CSS pseudo-class, we have to
+// explicitly set the valid/invalid class on our email field
+window.addEventListener("load", () => {
+  // Here, we test if the field is empty (remember, the field is not required)
+  // If it is not, we check if its content is a well-formed email address.
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  email.className = isValid ? "valid" : "invalid";
+});
+
+// This defines what happens when the user types in the field
+email.addEventListener("input", () => {
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (isValid) {
+    email.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+  } else {
+    email.className = "invalid";
+  }
+});
+
+// This defines what happens when the user tries to submit the data
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (!isValid) {
+    email.className = "invalid";
+    error.textContent = "Enter email address in lowercase!";
+    error.className = "error active";
+  } else {
+    email.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+    form.submit();
+  }
+});
