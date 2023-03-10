34d087fb3c19d597f4013cf0fb356cfd05a5ccf3
Harriet Oteng <harriet.oteng@gmail.com>
2023-03-10 09:22:35 +0000

    Add error message

--- portfolio.js
+++ portfolio.js
@@ -217,17 +217,49 @@ closeBtns.forEach((x) => {
 
 // form validation
 
-// This uses the input event on both inputs, and when one is not empty it sets the required property of the other input to false.
-document.addEventListener('DOMContentLoaded', function() {
-  const inputs = Array.from(
-    document.querySelectorAll('input[name=user_message], input[name=user_message_desktop]')
-  );
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const error = document.getElementById("message");
 
-  const inputListener = e => {
-    inputs
-      .filter(i => i !== e.target)
-      .forEach(i => (i.required = !e.target.value.length));
-  };
+// As per the HTML Specification
+const emailRegExp =
+  /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
 
-  inputs.forEach(i => i.addEventListener('input', inputListener));
+// Now we can rebuild our validation constraint
+// Because we do not rely on CSS pseudo-class, we have to
+// explicitly set the valid/invalid class on our email field
+window.addEventListener("load", () => {
+  // Here, we test if the field is empty (remember, the field is not required)
+  // If it is not, we check if its content is a well-formed email address.
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  email.className = isValid ? "valid" : "invalid";
+});
+
+// This defines what happens when the user types in the field
+email.addEventListener("input", () => {
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (isValid) {
+    email.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+  } else {
+    email.className = "invalid";
+  }
+});
+
+// This defines what happens when the user tries to submit the data
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (!isValid) {
+    email.className = "invalid";
+    error.textContent = "Enter a valid email address. Use lowercase";
+    error.className = "error active";
+  } else {
+    email.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+    form.submit();
+  }
 });
